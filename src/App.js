601e4496d5070ad430f2a353d10e5c4dbb9c2c02
lachraf <achrafsallemsfaxiano@gmail.com>
2022-02-24 03:59:18 +0100

    react proj first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+import { FormSlide } from './components/FormSlide';
+import { Tableee } from './components/Tableee';
+import { Formm } from './components/Formm';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React JS</h1>
+      <NavBar/>
+      <FormSlide/>
+      <h2>Students Names</h2>
+<Tableee/>
+      <Formm/>
     </div>
   );
 }
